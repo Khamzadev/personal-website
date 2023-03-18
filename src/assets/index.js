6d186d6e055ed294de850fe694d95602063c0ade
Khamza Avazbekov <khamzadevv@gmail.com>
2023-03-18 18:18:20 +0600

    add my personal project

---
+++ src/assets/index.js
@@ -0,0 +1,85 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import mui from './tech/mui.png';
+import sass from './tech/scss.png';
+import vscode from './tech/vscode.png';
+import chartjs from './tech/chart.svg'
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import neobis from './company/Neobis.png';
+import navis from './company/navis.jfif';
+import itRun from './company/itrun.jfif'
+import itrunKids from './company/itrunKids.avif'
+import oralce from './company/oracle.jfif';
+import growave from './company/growave.jfif';
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import crm from './tech/CRM.png';
+import silk from './tech/silk.jfif';
+import domket from './tech/domket.png';
+
+export {
+  chartjs, crm, silk,domket,
+  vscode,
+  growave,
+  oralce,
+  itrunKids,
+  itRun,
+  neobis,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  navis,
+  html,
+  javascript,
+  mongodb,
+  mui,
+  sass,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,324 @@
+import {
+  domket,
+  silk,
+  crm,
+  chartjs,
+  vscode,
+  sass,
+  mui,
+  growave,
+  oralce,
+  itrunKids,
+  mobile,
+  itRun,
+  backend,
+  navis,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  neobis
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Self-motivated",
+    icon: web,
+  },
+  {
+    title: "Effective team player",
+    icon: mobile,
+  },
+  {
+    title: "+2 years of experience",
+    icon: backend,
+  },
+  {
+    title: "Good time management skills",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Material UI",
+    icon: mui,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "SCSS/SASS/LESS",
+    icon: sass,
+  },
+  {
+    name: "Chart JS",
+    icon: chartjs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "VSCODE",
+    icon: vscode,
+  },
+];
+
+const experiences = [
+  {
+    title: "Front-end Engineer",
+    company_name: "Neobis",
+    icon: neobis,
+    iconBg: "#383E56",
+    date: "May 2022 - April 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Front-end Engineer",
+    company_name: "Navis",
+    icon: navis,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Oct 2022",
+    points: [
+      "Development of internal projects of the company",
+      "Created simple pages using only HTML and CSS, and more complex interfaces, using React and various APIs",
+      "Development of projects in a team using the Agile methodology.",
+      "Mentoring in the development of mentee projects and code reviews",
+      "Team development using Scrum, Trello, Git Flow, Slack",
+    ],
+  },
+  {
+    title: "Frontend Engineer",
+    company_name: "Oracle Digital",
+    icon: oralce,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Jul 2022",
+    points: [
+      "The Domket.kg project is a marketplace for the sale and rental of apartments / houses / plots, and services between buyer and client",
+      "Development of a site based on the analysis of the market, markets and offers.",
+      "Creation of a site for all platform requirements",
+      "Create, edit, delete, comment ads",
+      "RIntegration with connecting networks facebook, google (authorization)",
+      "atalog of ads with filtering and search, division of ads by prices and areas  objects",
+      "Localization of web applications using the i18n library."
+    ],
+  },
+  {
+    title: "Intern Front-end Engineer",
+    company_name: "Growave",
+    icon: growave,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Jul 2022",
+    points: [
+      "Processing customer requests and providing technical support.",
+      "Performed maintenance and updates of the current      web applications.",
+      "Writing cross-browser HTML, CSS and JavaScript.",
+      "Identification and elimination of software deficiencies.",
+      "Exploring the liquid library for working with products in Shopify.",
+    ],
+  },
+  {
+    title: "Frontend development Teacher",
+    company_name: "IT-RUN",
+    icon: itRun,
+    iconBg: "#383E56",
+    date: "Dec 2022 - Aug 2023 ",
+    points: [
+      "Improving the stage plan",
+      "Conducting consultations and analysis of assignments for students",
+      "Participation in meetings of mentors and reviewers",
+      "Close work with the head of the direction and mentor in the learning process.",
+    ],
+  },
+  {
+    title: "Teaching assistant for Frontend development",
+    company_name: "IT-RUN",
+    icon: itRun,
+    iconBg: "#E6DEDD",
+    date: "Oct 2021 - Sep 2022",
+    points: [
+      "Checking student progress",
+      "Assistance to the mentor in academic affairs",
+      "Checking homework and feedback to students",
+      "Respond to students' messages and questions in the chat",
+      "Improvement of the curriculum."
+    ],
+  },
+  {
+    title: "Frontend development Teacher for teeand and kids",
+    company_name: "IT-RUN KIDS",
+    icon: itrunKids,
+    iconBg: "#E6DEDD",
+    date: "Apr 2021 - Oct 2021",
+    points: [
+      "Teaching children the basics of HTML/CSS, JS",
+      "Help students in learning",
+      "Checking homework and feedback to students",
+      "Respond to students' messages and questions in the chat",
+      "Released more than 30 children"
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Kuhbak Mamytov",
+    designation: "Teamled Front-end Development",
+    company: "Neobis",
+    image: "https://media.licdn.com/dms/image/C5603AQHktLlMkxVYCA/profile-displayphoto-shrink_800_800/0/1620016847307?e=2147483647&v=beta&t=77WSS0lEw1KeJ3vlGqE-dUouX8cT_402U2KEZjbV2w0",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Islam Mainazarov",
+    designation: "Front-end Developer",
+    company: "Neobis",
+    image: "https://avatars.githubusercontent.com/u/56814948?v=4",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Ruslan Sabitov",
+    designation: "Front-end Developer",
+    company: "Neobis",
+    image: "https://levardo.com/gallery/sun9-6.userapi.com/c855324/v855324621/14e19a/0ItCCqdyFzA.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "CMR-LABS",
+    description:
+      "CMS is an application for recording and storing data about all clients coming through different  advertising, as well as storage of data about students, teachers and groups and the output of analytics",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux-toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: crm,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "SilkRoad Trip",
+    description:
+      "SilkRoad Trip is a comprehensive application that has been specifically designed to enhance the travel experience of those visiting Kyrgyzstan for vacation or leisure purposes",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux toolkit",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: silk,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Domket",
+    description:
+      "Domket.kg is a comprehensive project that serves as an online marketplace for individuals looking to buy or rent apartments, houses, and plots in Kyrgyzstan. The platform also offers a range of services",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: domket,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
